e8170154e4a7049601f323747ab6f1aa43d3bf50
jorijnsmit <mail@gosuto.ai>
2021-12-30 10:00:28 +0000

    typos in failing tests after 1e6883c

--- tests/aave/test_aave_withdraw.py
+++ tests/aave/test_aave_withdraw.py
@@ -8,11 +8,11 @@ def deposited(aave, USDC):
 def test_withdraw(safe, aave, USDC, aUSDC):
     bal_before_usdc = USDC.balanceOf(safe)
     bal_before_ausdc = aUSDC.balanceOf(safe)
-    
+
     to_withdraw = 1000 * 10**USDC.decimals()
 
     aave.withdraw(USDC, to_withdraw)
-    
+
     # aave will sometimes give you slightly more/less than what you asked for, so test >=
     assert USDC.balanceOf(safe) >= bal_before_usdc + to_withdraw
     assert aUSDC.balanceOf(safe) >= bal_before_ausdc - to_withdraw
@@ -24,5 +24,5 @@ def test_withdraw_all(safe, aave, USDC, aUSDC):
 
     aave.withdraw_all(USDC)
 
-    assert USDC.balanceOf(safe) < bal_before_usdc + bal_before_ausdc
+    assert USDC.balanceOf(safe) >= bal_before_usdc + bal_before_ausdc
     assert aUSDC.balanceOf(safe) == 0
