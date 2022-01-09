820c53852bb24a950fc308cb6bc8c9669e570e38
jorijnsmit <mail@gosuto.ai>
2022-01-09 11:28:13 +0000

    add mintable tokens to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+from brownie_tokens import MintableForkToken
+
 from great_ape_safe import GreatApeSafe
 
 
@@ -12,9 +14,29 @@ def shared_setup(module_isolation):
 def safe():
     return GreatApeSafe('dev.badgerdao.eth')
 
+
+@pytest.fixture
+def alusd(safe):
+    alusd = MintableForkToken(
+        '0xBC6DA0FE9aD5f3b0d58160288917AA56653660E9', owner=safe.account
+    )
+    alusd._mint_for_testing(safe.address, 10_000 * 10**alusd.decimals())
+    return alusd
+
+
+@pytest.fixture
+def aleth(safe):
+    aleth = MintableForkToken(
+        '0x0100546F2cD4C9D97f798fFC9755E47865FF7Ee6', owner=safe.account
+    )
+    aleth._mint_for_testing(safe.address, 10_000 * 10**aleth.decimals())
+    return aleth
+
+
 @pytest.fixture
 def USDC(safe):
     return safe.contract('0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48')
+
 
 # Aave fixtures
 @pytest.fixture
@@ -22,13 +44,16 @@ def aave(safe):
     safe.init_aave()
     return safe.aave
 
+
 @pytest.fixture
 def aUSDC(safe):
     return safe.contract('0xBcca60bB61934080951369a648Fb03DF4F96263C')
 
+
 @pytest.fixture
 def sktAAVE(safe):
     return safe.contract('0x4da27a545c0c5B758a6BA100e3a049001de870f5')
+
 
 @pytest.fixture
 def AAVE(safe):
@@ -41,65 +66,84 @@ def curve(safe):
     safe.init_curve()
     return safe.curve
 
+
 @pytest.fixture
 def registry(safe):
     provider = safe.contract('0x0000000022D53366457F9d5E68Ec105046FC4383')
     return safe.contract(provider.get_registry())
 
+
 @pytest.fixture
 def tripool_address(registry):
     return registry.pool_list(0)
+
 
 @pytest.fixture
 def tripool_lptoken(safe, registry):
     tripool = safe.contract(registry.pool_list(0))
     return safe.contract(registry.get_lp_token(tripool))
 
+
 @pytest.fixture
 def threepool_lp(safe):
     return safe.contract('0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490')
 
+
 @pytest.fixture
 def CRV(safe):
     return safe.contract('0xD533a949740bb3306d119CC777fa900bA034cd52')
-    
 
-# Compound Fixtures
+
+# Compound fixtures
 @pytest.fixture
 def compound(safe):
     safe.init_compound()
     return safe.compound
 
+
 @pytest.fixture
 def cUSDC(safe):
     return safe.contract('0x39AA39c021dfbaE8faC545936693aC917d5E7563')
+
 
 @pytest.fixture
 def cETH(safe):
     return safe.contract('0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5')
 
+
 @pytest.fixture
 def COMP(safe):
     return safe.contract('0xc00e94Cb662C3520282E6f5717214004A7f26888')
 
-# Convex
+
+# Convex fixtures
 @pytest.fixture
 def convex(safe):
     safe.init_convex()
     return safe.convex
 
+
 @pytest.fixture
 def convex_rewards(safe, convex, threepool_lp):
-    (_,_,_,rewards ) = convex.get_pool_info(threepool_lp)
+    _, _, _, rewards = convex.get_pool_info(threepool_lp)
     return safe.contract(rewards)
+
 
 @pytest.fixture
 def convex_threepool_lp(safe, convex, threepool_lp):
-    (_,token,_,_ ) = convex.get_pool_info(threepool_lp)
+    _, token, _, _ = convex.get_pool_info(threepool_lp)
     return safe.contract(token)
+
 
 @pytest.fixture
 def convex_threepool_reward(safe, convex, threepool_lp):
-    (_,_,_,rewards ) = convex.get_pool_info(threepool_lp)
+    _, _, _, rewards = convex.get_pool_info(threepool_lp)
     reward = safe.contract(rewards).rewardToken()
     return safe.contract(reward)
+
+
+# Saddle fixtures
+@pytest.fixture
+def saddle(safe):
+    safe.init_saddle()
+    return safe.saddle
